39268e762470a8d93318b95fca8f10869e0452b4
Codezigineer <iamjojozm@icloud.com>
2025-07-24 00:59:40 -0700

    grep error

--- scripts/alpine.sh
+++ scripts/alpine.sh
@@ -1,6 +1,6 @@
 export PATH=/bin
 arch=$(/bin/uname -m)
-latest=$(/bin/curl https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/$arch/latest-releases.yaml | /bin/grep "version: \\d*.\\d*.\\d*" | /bin/head -1)
+latest=$(/bin/curl https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/$arch/latest-releases.yaml | /bin/grep "version: [0-9]*.[0-9]*.[0-9]*" | /bin/head -1)
 version=${latest:11}
 url="https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/$arch/alpine-minirootfs-$version-$arch.tar.gz"
 /bin/curl $url -o alpine.tar.gz
